b85ead8143a2b312c920a018e113a276e5591851
Brainzeater <brainzeater@gmail.com>
2020-02-12 11:33:30 +0300

    Game Manager Prefab
    - Box prefab instead of Rectangle
    - BoxSize scriptable object
    - Game Manager prefab
    - Box layer
    - Game Manager uses Box prefab, BoxSize and Box layer to check if there's enough space for a new box and to spawn it

---
+++ DraggingRects/Assets/Scripts/BoxSize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// Stores the data about width and height of the box
+[CreateAssetMenu(fileName = "BoxSize", menuName = "BoxSize")]
+public class BoxSize : ScriptableObject
+{
+    public float width;
+    public float height;
+}
---
+++ DraggingRects/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager _instance;
+    public static GameManager Instance => _instance;
+
+    public GameObject box;
+    public BoxSize size;
+
+    public LayerMask layer;
+
+    void Awake()
+    {
+        // Singleton pattern
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckSpace();
+        }
+    }
+
+    /*
+     * Spawns a rect if there's enough space for it
+     */
+    void CheckSpace()
+    {
+        Vector2 pointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (!Physics2D.Raycast(pointerPosition, Vector2.zero))
+        {
+            // Rectangular area coordinates
+            Vector2 pointA = new Vector2(pointerPosition.x - size.width / 2, pointerPosition.y - size.height / 2);
+            Vector2 pointB = new Vector2(pointerPosition.x + size.width / 2, pointerPosition.y + size.height / 2);
+
+            if (!Physics2D.OverlapArea(pointA, pointB, layer))
+            {
+                SpawnBox(pointerPosition);
+            }
+        }
+    }
+
+    void SpawnBox(Vector2 position)
+    {
+        GameObject boxGameObject = Instantiate(box, position, Quaternion.identity);
+
+        // Set size of created box
+        boxGameObject.transform.localScale = new Vector3(size.width, size.height, 1f);
+    }
+}
