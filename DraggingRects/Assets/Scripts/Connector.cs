ad378a4c5805d7f9010b506cd4b2cb4c22813dc4
Brainzeater <brainzeater@gmail.com>
2020-02-13 20:16:17 +0300

    Connection remove support
    - Connection can be removed on a second attempt to connect the boxes
    - Custom Key to activate connection mode
    - Comments and code structure update

--- DraggingRects/Assets/Scripts/Connector.cs
+++ DraggingRects/Assets/Scripts/Connector.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/*
+ * Enables the connection of the boxes when the key is pressed (space by default).
+ * When the two boxes are selected, the new connection is created between them.
+ * Connections are translated with the boxes.
+ * Repeat of the connection destroys the existing one.
+ * Connections are destroyed with the boxes they belong to.
+ */
 public class Connector : MonoBehaviour
 {
     // Stores the number of boxes selected for the connection
-    private static int boxCounter = 0;
+    private static int selectedBoxCounter = 0;
 
     // Stores the first box selected for the connection
     private static GameObject connectionStart;
+
+    // Key to activate connection mode
+    public KeyCode key;
 
     // Line prefab
     public GameObject line;
 
     // All lines with other boxes
     public List<ConnectionAndRole> LineCollection { get; set; }
+
 
     void Awake()
     {
@@ -24,69 +35,90 @@ public class Connector : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(key))
         {
-            boxCounter = 0;
+            selectedBoxCounter = 0;
         }
     }
 
     void OnMouseDown()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(key))
         {
-            if (boxCounter < 1)
+            if (selectedBoxCounter < 1)
             {
                 connectionStart = this.gameObject;
-                boxCounter++;
+                selectedBoxCounter++;
             }
 
+            // The box cannot be connected with itself
             else if (this.gameObject.GetInstanceID() != connectionStart.gameObject.GetInstanceID())
             {
-                CreateNewConnection();
-                boxCounter = 0;
+                CheckConnection();
+                selectedBoxCounter = 0;
             }
         }
 
+        // Clear the list from null lines (when they were destroyed by the other box)
         if (LineCollection.Any())
         {
             LineCollection.RemoveAll(item => item.line == null);
         }
     }
 
-    void CreateNewConnection()
+    void CheckConnection()
     {
         bool alreadyConnected = false;
-
+        int connectionID = -1;
         foreach (ConnectionAndRole connection in LineCollection)
         {
             if (connection.otherSideId == connectionStart.gameObject.GetInstanceID())
             {
                 alreadyConnected = true;
+                connectionID = connectionStart.gameObject.GetInstanceID();
                 break;
             }
         }
 
         if (!alreadyConnected)
         {
-            GameObject connectionLine = Instantiate(line);
-
-            LineRenderer renderer = connectionLine.GetComponent<LineRenderer>();
-
-            // Set start
-            renderer.SetPosition(0, connectionStart.transform.position);
-            renderer.startColor = connectionStart.GetComponent<SpriteRenderer>().color;
-            connectionStart.GetComponent<Connector>().LineCollection
-                .Add(new ConnectionAndRole(renderer, Role.Start, this.gameObject.GetInstanceID()));
-
-            // Set end
-            renderer.SetPosition(1, gameObject.transform.position);
-            renderer.endColor = GetComponent<SpriteRenderer>().color;
-            LineCollection.Add(
-                new ConnectionAndRole(renderer, Role.End, connectionStart.gameObject.GetInstanceID()));
+            CreateNewConnection();
+        }
+        else
+        {
+            // Destroy the connection and remove it from the list
+            Destroy(LineCollection.Find(item => item.otherSideId == connectionID).line.gameObject);
+            LineCollection.RemoveAll(item => item.otherSideId == connectionID);
         }
     }
 
+    void CreateNewConnection()
+    {
+        GameObject connectionLine = Instantiate(line);
+        LineRenderer lineRenderer = connectionLine.GetComponent<LineRenderer>();
+
+        // Set start of the line and update the list of connections
+        lineRenderer.SetPosition(0, connectionStart.transform.position);
+        lineRenderer.startColor = connectionStart.GetComponent<SpriteRenderer>().color;
+        connectionStart.GetComponent<Connector>().LineCollection
+            .Add(new ConnectionAndRole(lineRenderer, Role.Start, this.gameObject.GetInstanceID()));
+
+        // Set end of the line and update the list of connections
+        lineRenderer.SetPosition(1, gameObject.transform.position);
+        lineRenderer.endColor = GetComponent<SpriteRenderer>().color;
+        LineCollection.Add(
+            new ConnectionAndRole(lineRenderer, Role.End, connectionStart.gameObject.GetInstanceID()));
+
+        // TODO: Reset double-click
+    }
+
     void OnMouseDrag()
+    {
+        DragLines();
+    }
+
+    // Drags the lines depending on the role of the box
+    void DragLines()
     {
         if (LineCollection.Any())
         {
@@ -107,6 +139,7 @@ public class Connector : MonoBehaviour
 
     void OnDestroy()
     {
+        // Destroy the connections and clear the list from null lines
         if (LineCollection.Any())
         {
             LineCollection.RemoveAll(item => item.line == null);
@@ -116,7 +149,7 @@ public class Connector : MonoBehaviour
             }
         }
 
-        boxCounter = 0;
+        selectedBoxCounter = 0;
     }
 
     public enum Role
@@ -125,6 +158,12 @@ public class Connector : MonoBehaviour
         End
     }
 
+    /*
+     * Stores the info about the connection.
+     * line - to update one of the line's positions
+     * role - to figure out which line's position to update
+     * otherSideId - to check if the connection already exists
+     */
     public struct ConnectionAndRole
     {
         public LineRenderer line;
