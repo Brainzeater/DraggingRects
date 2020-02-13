8e0bbacf28fabac498c4c2eca8344b405a9f3858
Brainzeater <brainzeater@gmail.com>
2020-02-13 19:25:51 +0300

    Multiple connections support
    - ConnectionAndRole struct stores the info about the connection (LineRenderer, Role of the box, Id of the other box)
    - Box supports multiple connections with list of ConnectionAndRole
    - Boxes can't be self-connected
    - Two boxes can't be connected more than once
    - Proper connection destruction

--- DraggingRects/Assets/Scripts/Connector.cs
+++ DraggingRects/Assets/Scripts/Connector.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Connector : MonoBehaviour
 {
+    // Stores the number of boxes selected for the connection
     private static int boxCounter = 0;
 
+    // Stores the first box selected for the connection
+    private static GameObject connectionStart;
+
+    // Line prefab
     public GameObject line;
 
-    private static GameObject connectionStart;
-    private Role _role;
-    
-    // TODO: Update to list
-    public LineRenderer LineRenderer { get; set; }
+    // All lines with other boxes
+    public List<ConnectionAndRole> LineCollection { get; set; }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        LineCollection = new List<ConnectionAndRole>();
+    }
+
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
             boxCounter = 0;
-            Debug.Log($"UFF {boxCounter}");
         }
     }
 
@@ -30,52 +36,87 @@ public class Connector : MonoBehaviour
         {
             if (boxCounter < 1)
             {
-                _role = Role.Start;
                 connectionStart = this.gameObject;
                 boxCounter++;
-                Debug.Log($"WOW {GetInstanceID()} {boxCounter}");
             }
-//            else if (_role != Role.Start)
-            else 
+
+            else if (this.gameObject.GetInstanceID() != connectionStart.gameObject.GetInstanceID())
             {
-                _role = Role.End;
-                GameObject connection = Instantiate(line);
-                LineRenderer = connection.GetComponent<LineRenderer>();
-                connectionStart.GetComponent<Connector>().LineRenderer = connection.GetComponent<LineRenderer>();
-                // Set start
-                LineRenderer.SetPosition(0, connectionStart.transform.position);
-                LineRenderer.startColor = connectionStart.GetComponent<SpriteRenderer>().color;
-                // Set end
-                LineRenderer.SetPosition(1, gameObject.transform.position);
-                LineRenderer.endColor = GetComponent<SpriteRenderer>().color;
+                CreateNewConnection();
                 boxCounter = 0;
             }
+        }
+
+        if (LineCollection.Any())
+        {
+            LineCollection.RemoveAll(item => item.line == null);
+        }
+    }
+
+    void CreateNewConnection()
+    {
+        bool alreadyConnected = false;
+
+        foreach (ConnectionAndRole connection in LineCollection)
+        {
+            if (connection.otherSideId == connectionStart.gameObject.GetInstanceID())
+            {
+                alreadyConnected = true;
+                break;
+            }
+        }
+
+        if (!alreadyConnected)
+        {
+            GameObject connectionLine = Instantiate(line);
+
+            LineRenderer renderer = connectionLine.GetComponent<LineRenderer>();
+
+            // Set start
+            renderer.SetPosition(0, connectionStart.transform.position);
+            renderer.startColor = connectionStart.GetComponent<SpriteRenderer>().color;
+            connectionStart.GetComponent<Connector>().LineCollection
+                .Add(new ConnectionAndRole(renderer, Role.Start, this.gameObject.GetInstanceID()));
+
+            // Set end
+            renderer.SetPosition(1, gameObject.transform.position);
+            renderer.endColor = GetComponent<SpriteRenderer>().color;
+            LineCollection.Add(
+                new ConnectionAndRole(renderer, Role.End, connectionStart.gameObject.GetInstanceID()));
         }
     }
 
     void OnMouseDrag()
     {
-        if (LineRenderer != null)
+        if (LineCollection.Any())
         {
-            switch (_role)
+            foreach (ConnectionAndRole connection in LineCollection)
             {
-                case Role.Start:
-                    LineRenderer.SetPosition(0, gameObject.transform.position);
-                    break;
-                case Role.End:
-                    LineRenderer.SetPosition(1, gameObject.transform.position);
-                    break;
+                switch (connection.role)
+                {
+                    case Role.Start:
+                        connection.line.SetPosition(0, gameObject.transform.position);
+                        break;
+                    case Role.End:
+                        connection.line.SetPosition(1, gameObject.transform.position);
+                        break;
+                }
             }
         }
     }
 
     void OnDestroy()
     {
-        if(LineRenderer != null)
+        if (LineCollection.Any())
         {
-            Destroy(LineRenderer.gameObject);
+            LineCollection.RemoveAll(item => item.line == null);
+            foreach (ConnectionAndRole connection in LineCollection)
+            {
+                Destroy(connection.line.gameObject);
+            }
         }
 
+        boxCounter = 0;
     }
 
     public enum Role
@@ -84,9 +125,17 @@ public class Connector : MonoBehaviour
         End
     }
 
-//    private struct ConnectionAndRole
-//    {
-//        private LineRenderer line;
-//        private Role role;
-//    }
+    public struct ConnectionAndRole
+    {
+        public LineRenderer line;
+        public Role role;
+        public int otherSideId;
+
+        public ConnectionAndRole(LineRenderer line, Role role, int otherSideId)
+        {
+            this.line = line;
+            this.role = role;
+            this.otherSideId = otherSideId;
+        }
+    }
 }
