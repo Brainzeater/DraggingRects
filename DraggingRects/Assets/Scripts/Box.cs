a7393c7c9074e06c05e983506bed85648f8a0628
Brainzeater <brainzeater@gmail.com>
2020-02-13 14:11:47 +0300

    Boxes collide with each other on dragging
    - Box spawner moved back in Z-axis
    - Box spawner is on a new layer to avoid collision with Boxes
    - Boxes have a rigidbody now (static by default/dynamic on drag)
    - Boxes dragging mechanic update

--- DraggingRects/Assets/Scripts/Box.cs
+++ DraggingRects/Assets/Scripts/Box.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Box : MonoBehaviour
 {
     private const float DoubleClickInterval = 0.5f;
     private bool _doubleClickCounterStarted;
 
-    private bool _selected;
-    
-    // Distance between cursor and box center at the start of dragging
-    // This variable enables dragging by any point of the box
-    private Vector2 _cursorCenterDistance;
+    private const float DragSpeed = 30f;
+    private const float DragMaxSpeed = 80f;
 
-    void Awake()
+    // Mouse cursor position relative to the box center at the start of dragging
+    // This variable enables dragging the box by any point of the box
+    private Vector3 _cursorCenterOffsetPosition;
+
+    private Rigidbody2D _rigidbody;
+
+    void Start()
     {
         // Random color assignment
         GetComponent<SpriteRenderer>().color = new Color(
@@ -25,15 +30,15 @@ public class Box : MonoBehaviour
         _doubleClickCounterStarted = false;
         Debug.Log($"Hello from {GetInstanceID()}");
 
-        _selected = false;
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void OnMouseDown()
     {
         if (!_doubleClickCounterStarted)
         {
-            _selected = true;
-            _cursorCenterDistance = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            // Save relative position of mouse click point to the box center
+            _cursorCenterOffsetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             StartCoroutine(DoubleClickCounter());
         }
         else
@@ -43,20 +48,43 @@ public class Box : MonoBehaviour
         }
     }
 
+
+    void OnMouseDrag()
+    {
+        // Enable box to be movable by velocity
+        _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        
+        // Keep the cursor offset
+        Vector3 boxWithCursorOffsetPosition = transform.position + _cursorCenterOffsetPosition;
+
+        // The following code is inspired by:
+        // http://answers.unity.com/answers/149435/view.html
+
+        // Cast a ray from the mouse cursor
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Get distance from the mouse pointer
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(cursorPosition, Vector2.zero);
+        float length = hit.distance;
+
+        // Calculate velocity necessary to follow the mouse cursor
+        Vector2 velocity = (ray.GetPoint(length) - boxWithCursorOffsetPosition) * DragSpeed;
+
+        // Limit max velocity to avoid pass through other boxes
+        if (velocity.magnitude > DragMaxSpeed) velocity *= DragMaxSpeed / velocity.magnitude;
+
+        // Set box velocity
+        _rigidbody.velocity = velocity;
+    }
+
     void OnMouseUp()
     {
-        _selected = false;
+        // Reset rigid body to  
+        _rigidbody.velocity = Vector2.zero;
+        _rigidbody.bodyType = RigidbodyType2D.Static;
     }
-
-    void Update()
-    {
-        if (_selected)
-        {
-            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = cursorPosition - _cursorCenterDistance;
-        }
-    }
-
+    
     // Double click interval timer
     private IEnumerator DoubleClickCounter()
     {
